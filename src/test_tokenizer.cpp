fd5fcfa3c59639a2f0672633ba34dbb05dd78804
Theodore Corrello-Reiser <59662884+scottycr@users.noreply.github.com>
2022-05-09 12:09:14 -0600

    Started Tokenizer tests

---
+++ src/test_tokenizer.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+#include "tokenizer.h"
+
+using namespace std;
+
+using namespace tokenizer;
+
+TEST(TokenizerScanSymbols, Plus) {
+    Tokenizer t("+");
+    ASSERT_EQ(t.scan(), PLUS);
+}
+
+TEST(TokenizerScanSymbols, Minus) {
+    Tokenizer t("-");
+    ASSERT_EQ(t.scan(), MINUS);
+}
+
+TEST(TokenizerScanSymbols, Less) {
+    Tokenizer t("<");
+    ASSERT_EQ(t.scan(), LESS);
+}
+
+TEST(TokenizerScanSymbols, Greater) {
+    Tokenizer t(">");
+    ASSERT_EQ(t.scan(), GREATER);
+}
+
+TEST(TokenizerScanSymbols, Dot) {
+    Tokenizer t(".");
+    ASSERT_EQ(t.scan(), DOT);
+}
+
+TEST(TokenizerScanSymbols, Comma) {
+    Tokenizer t(",");
+    ASSERT_EQ(t.scan(), COMMA);
+}
+
+TEST(TokenizerScanSymbols, OpenBracket) {
+    Tokenizer t(" [");
+    ASSERT_EQ(t.scan(), OPENBRACKET);
+}
+
+TEST(TokenizerScanSymbols, CloseBracket) {
+    Tokenizer t("]");
+    ASSERT_EQ(t.scan(), CLOSEBRACKET);
+}
+
+TEST(TokenizerScanSymbols, End) {
+    Tokenizer t(" ");
+    ASSERT_EQ(t.scan(), END);
+}
+
+TEST(TokenizerScan, IgnoreCommentLoop) {
+    Tokenizer t("[This is a comment loop] +");
+    ASSERT_EQ(t.scan(), PLUS);
+}
