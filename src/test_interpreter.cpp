3cf02e8e179877cac2f92f4afd4ef7b97eb6b2f9
Theodore Corrello-Reiser <59662884+scottycr@users.noreply.github.com>
2022-05-11 14:31:24 -0600

    Finished tests for interpreter

--- src/test_interpreter.cpp
+++ src/test_interpreter.cpp
@@ -1,15 +1,71 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+
 #include <gtest/gtest.h>
 
 #include "interpreter.h"
 #include "tokenizer.h"
+#include "parser.h"
 #include "celltable.h"
 
 using namespace std;
 
 using namespace interpreter;
 using namespace tokenizer;
+using namespace parser;
 using namespace celltable;
 
-TEST(A, A) {
-    ASSERT_EQ(1, 1);
+stringstream strin;
+ostringstream strout;
+fstream fin;
+
+TEST(Interpreter, HelloWorld) {
+    fin.open("./examples/hello.bf");
+    strout << fin.rdbuf();
+    fin.close();
+
+    Tokenizer t(strout.str(), "hello.bf");
+    strout.str("");
+    vector<Symbol> program;
+    parse(t, program);
+
+    CellTable ct;
+    eval(program, ct, strout, strin);
+    string expectedResult = "Hello World!\n";
+    ASSERT_EQ(strout.str(), expectedResult);
+}
+
+TEST(Interpreter, Addition) {
+    fin.open("./examples/addition.bf");
+    strout << fin.rdbuf();
+    fin.close();
+
+    Tokenizer t(strout.str(), "addition.bf");
+    strout.str("");
+    vector<Symbol> program;
+    parse(t, program);
+
+    CellTable ct;
+    eval(program, ct, strout, strin);
+    string expectedResult = "7";
+    (strout.str(), expectedResult);
+}
+
+TEST(Interpreter, Cat) {
+    fin.open("./examples/cat.bf");
+    strout << fin.rdbuf();
+    fin.close();
+
+    Tokenizer t(strout.str(), "cat.bf");
+    strout.str("");
+    vector<Symbol> program;
+    parse(t, program);
+
+    CellTable ct;
+    // \3 is to inser Ctrl + C into the program 
+    strin << "hello\3";
+    eval(program, ct, strout, strin);
+    string expectedResult = "hello\3";
+    ASSERT_EQ(strout.str(), expectedResult);
 }
