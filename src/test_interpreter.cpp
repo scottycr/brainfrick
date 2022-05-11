1d400dc8ed55116e9082cd721739cc87ce31a66d
Theodore Corrello-Reiser <59662884+scottycr@users.noreply.github.com>
2022-05-11 09:42:17 -0600

    Created interpreter tests

---
+++ src/test_interpreter.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "interpreter.h"
+#include "tokenizer.h"
+#include "celltable.h"
+
+using namespace std;
+
+using namespace interpreter;
+using namespace tokenizer;
+using namespace celltable;
+
+TEST(A, A) {
+    ASSERT_EQ(1, 1);
+}
